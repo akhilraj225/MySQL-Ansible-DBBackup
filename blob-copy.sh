cbd99716355978b5c062402162aabb016af6fb59
Ubuntu <appadmin@DevAgentMachine.tjikbd3yqooe1kqico44gohdre.ax.internal.cloudapp.net>
2022-03-19 07:02:53 +0000

    Database backup script

---
+++ blob-copy.sh
@@ -0,0 +1,2 @@
+#/bin/sh
+sudo ./azcopy copy "/home/appadmin/DB-BACKUP/*" "https://applabbackup.blob.core.windows.net/testakhil?sp=racwdl&st=2022-03-16T08:51:57Z&se=2022-03-26T16:51:57Z&sv=2020-08-04&sr=c&sig=eheZU%2Bqr2%2FlIUWI%2FeEItv7AYBspYUBbuQmI4jGhWSSE%3D"
